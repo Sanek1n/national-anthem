c817d21255951a00ca8c09ef715d3b7740bb383c
Alexandr Kukovskii <sanekin8ru@outlook.com>
2022-11-18 14:50:20 +0400

    feat: add styles, adaptive for result page

--- nationalanthem/src/pages/result/result.js
+++ nationalanthem/src/pages/result/result.js
@@ -10,3 +10,19 @@ for (let i = 0; i < playerScore.length; i++) {
 
 let sum = playerScore.reduce((sum, val) => sum + val, 0);
 document.querySelector('.score-5').textContent = sum;
+
+if (sum === 25) {
+  document.querySelector('.main__title').textContent = 'Поздравляю!!! Максимальный результат! \n Вы наверно гений?!';
+  document.querySelector('.main__repeat').textContent = 'Вы наверно гений?!';
+} else if(sum <= 24 && sum >= 20) {
+  document.querySelector('.main__title').textContent = 'Поздравляю!!! Отличный результат.';
+  document.querySelector('.main__repeat').textContent = 'Попробуйте еще раз.';
+} else if(sum <= 19 && sum >= 10) {
+  document.querySelector('.main__title').textContent = 'Поздравляю!!! Хороший результат.';
+  document.querySelector('.main__repeat').textContent = 'Попробуйте еще раз.';
+} else {
+  document.querySelector('.main__title').textContent = 'Поздравляю!!! Видимо это не ваше.';
+  document.querySelector('.main__repeat').textContent = 'Может попробуете еще раз?';
+}ж
+
+
